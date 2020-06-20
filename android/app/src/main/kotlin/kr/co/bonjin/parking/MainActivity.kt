ef3b1a7bb5d61348d681204b07724f7a9b1638ca
bonjin-app <bonjin.app@gmail.com>
2020-06-20 12:31:22 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/kr/co/bonjin/parking/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.co.bonjin.parking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/tab/tab_screen.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+import 'package:parking/screens/home/home_screen.dart';
+
+class TabScreen extends StatefulWidget{
+  @override
+  _TabScreenState createState() => _TabScreenState();
+}
+
+class _TabScreenState extends State<TabScreen> with WidgetsBindingObserver {
+  final Logger _logger = Logger();
+
+  PageController _pageController = PageController();
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: NeverScrollableScrollPhysics(),  // swipe 방지
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        children: <Widget>[
+          HomeScreen(),
+          Container(),
+          Container(),
+          Container(),
+          Container(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Theme.of(context).accentColor,
+        unselectedItemColor: Colors.grey.shade400,
+        selectedLabelStyle: TextStyle(
+          fontWeight: FontWeight.w600,
+        ),
+        unselectedLabelStyle: TextStyle(
+          fontWeight: FontWeight.w600,
+        ),
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          _pageController.jumpToPage(index);
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            title: Text("홈"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.menu,
+            ),
+            title: Text("카테고리"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.edit,
+            ),
+            title: Text("클쓰기"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.chat_bubble_outline,
+            ),
+            title: Text("채팅"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+            ),
+            title: Text("나의 주차"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// WidgetsBindingObserver
+  /// 시스템이 앱을 백그라운드에 놓거나 앱을 포 그라운드로 되돌릴 때 호출
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    switch (state) {
+      case AppLifecycleState.resumed:
+
+        _logger.d("didChangeAppLifecycleState : resumed state (ONLINE)");
+        break;
+
+      case AppLifecycleState.inactive:
+
+        _logger.d("didChangeAppLifecycleState : inactive state (OFFLINE)");
+        break;
+
+      case AppLifecycleState.paused:
+
+        _logger.d("didChangeAppLifecycleState : paused state (WAITING)");
+        break;
+
+      case AppLifecycleState.detached:
+
+        _logger.d("didChangeAppLifecycleState : detached state (OFFLINE)");
+        break;
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+}
